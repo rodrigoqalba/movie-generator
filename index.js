77ca97a236a34dcc09daf3e48e6ab6974bdb3282
Rodrigo Alba <rodrigoqalba@gmail.com>
2023-06-08 19:50:50 -0400

    basic js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const setupTextarea = document.getElementById('setup-textarea')
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
 
+const apiKey = ''
+const url = 'https://api.openai.com/v1/completions'
+
 document.getElementById("send-btn").addEventListener("click", () => {
-  if (setupTextarea.value) {
+  // if (setupTextarea.value) {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
-  }
+  // }
+  fetchBotReply()
 })
